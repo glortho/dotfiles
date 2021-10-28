3fa57abafd93d74f563073472b05dd40b8f0bc03
Jed Verity <glortho@github.com>
2021-10-28 16:08:49 -0400

    add missing unzip  Please enter the commit message for your changes. Lines starting

--- install.sh
+++ install.sh
@@ -143,6 +143,7 @@ sudo modprobe fuse
 sudo groupadd fuse
 sudo usermod -a -G fuse "$(whoami)"
 wget https://github.com/github/copilot.vim/releases/download/neovim-nightlies/appimage.zip
+unzip appimage.zip
 sudo chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
